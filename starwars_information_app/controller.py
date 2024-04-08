ed952e1ab00a9c91ed437f82f04d55a05de5f295
projects-programming <mcclm902@hsd.k12.or.us>
2024-04-08 09:39:07 -0700

    adding controller

---
+++ starwars_information_app/controller.py
@@ -0,0 +1,24 @@
+"""
+controller.py
+by Matthew McClelland
+Various functions for dealing with API calls and 
+data functionality
+"""
+
+# imports
+import requests as re
+
+def get_api_data(endpoint: str, query=""):
+    domain = "https://swapi.dev/api/"
+    url = domain + endpoint + "/" + query + "/"
+    response = re.get(url)
+
+    if response.ok:
+        results = response.text
+    else:
+        results = "Error!"
+    return results
+
+if __name__ == "__main__":
+    call = get_api_data("planets", "1")
+    print(call)
