b8dc35a0d11a35c1be4733cf0d010be403294a8e
projects-programming <mcclm902@hsd.k12.or.us>
2024-05-23 08:56:10 -0700

    adding labels to app

--- starwars_information_app/app.py
+++ starwars_information_app/app.py
@@ -7,7 +7,7 @@ from PyQt6.QtWidgets import (
     QLabel,
     QMainWindow,
     QPushButton,
-    QVBoxLayout,
+    QGridLayout,
     QWidget,
 )
 
@@ -19,10 +19,48 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Star Wars Information App")
         self.resize(640, 480)
-        layout = QVBoxLayout()
+        layout = QGridLayout()
+
+        # title label
+        title_label = QLabel("Star Wars Information")
+        font = title_label.font()
+        font.setPointSize(24)
+        title_label.setFont(font)
+        title_label.setStyleSheet("QLabel {color : #DDE6ED; }");
+
+        # people label
+        people_label = QLabel("People")
+        font = people_label.font()
+        font.setPointSize(18)
+        people_label.setFont(font)
+        people_label.setStyleSheet("QLabel {color : #DDE6ED; }");
+
+        # people combobox
+        
+
+        # starship label
+        starship_label = QLabel("Starships")
+        font = starship_label.font()
+        font.setPointSize(18)
+        starship_label.setFont(font)
+        starship_label.setStyleSheet("QLabel {color : #DDE6ED; }");
+
+        # planet label
+        planet_label = QLabel("Planets")
+        font = planet_label.font()
+        font.setPointSize(18)
+        planet_label.setFont(font)
+        planet_label.setStyleSheet("QLabel {color : #DDE6ED; }");
+
 
         widget = QWidget()
         widget.setLayout(layout)
+
+        # Add layout
+        layout.addWidget(title_label, 0, 1, Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(people_label, 1, 0, Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(starship_label, 1, 1, Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(planet_label, 1, 2, Qt.AlignmentFlag.AlignHCenter)
 
         # Set the central widget of the Window. Widget will expand
         # to take up all the space in the window by default.
